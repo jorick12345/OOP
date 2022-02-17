b24f3e39aea2a0166c77d715aa9136337c04ac28
jorick12345 <jorick.vanderhave@student.hu.nl>
2022-02-17 14:41:26 +0100

    practicum1

---
+++ pract1/practicum1opdracht5/Main.java
@@ -0,0 +1,15 @@
+package practicum1opdracht5;
+
+public class Main {
+    public static void main(String[] args){
+        for(int i = 0; i<4; i++){
+            if (i % 2 == 0){
+                System.out.println("s");
+            } else{
+                System.out.println("ss");
+            }
+        }
+
+
+    }
+}
